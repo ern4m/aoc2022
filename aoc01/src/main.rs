633f065f747bdc54e307bec135be25a1be383563
ern4m <ernam.rsf@gmail.com>
2023-06-16 09:21:13 -0300

    finished challenge 01

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -12,19 +12,23 @@ fn main() {
 
     let contents = fs::read_to_string(file_path).expect("Unable to read file!");
 
-    let biggest = 0;
-
-
+    let mut most_calories = 0;
 
     let sections = contents.split("\n");
+    let mut inventory_sum: i32 = 0;
     for section in sections {
-        let current: u32 = 0;
-        let mut newInventory: bool = false;
+        
         match section {
-            "" |" "| "\n" => { &newInventory = true }
+            "" |" "| "\n" => {inventory_sum = 0;},
+            _ => {
+                inventory_sum += section.parse::<i32>().unwrap();
+            },
         }
-        let parsed_section: u32 = section.parse().unwrap();
-        println!("Inventories = {}", section)
+        println!("Inventory sum: {}", inventory_sum);
+        if inventory_sum > most_calories {
+            most_calories = inventory_sum;
+        }
     }
+    println!("{most_calories}");
 }
 
