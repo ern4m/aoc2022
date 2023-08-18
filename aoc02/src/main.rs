c7c75fe8872e95c7884cdb0c62b8c450fab97973
ern4m <ernam.rsf@gmail.com>
2023-08-18 08:46:41 +0000

    Using structs and enums to solve the second challenge

--- aoc02/src/main.rs
+++ aoc02/src/main.rs
@@ -1,66 +1,141 @@
-use std::env;
-use std::fs;
-use bimap::{ BiMap, BiHashMap };
-use std::collections::HashMap;
 
-// Make function to read input and parse match pairs
-fn parse_file(file_path: &str) -> Vec<Vec<String>> {
-    let contents = fs::read_to_string(file_path).expect("Error while reading file");
-    contents
-        .lines()
-        .filter(|x| !x.is_empty())
-        .map(|x| {
-            x.split_whitespace()
-                .map(|x| x.to_string())
-                .collect::<Vec<_>>()
-        })
-        .collect()
+// implementing the data structures necessary for our data manipulation necessities
+
+use std::str::FromStr;
+
+#[derive(Debug, Clone, Copy)]
+enum Outcome {
+    Loss,
+    Draw,
+    Win,
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-
-    let parsed: Vec<Vec<String>> = parse_file(file_path);
-    println!("{:?}", parsed.len());
-
-    // A | X - Rock
-    // B | Y - Paper
-    // C | Z - Scissors
-    let mut equivalences: BiHashMap<&str, &str> = BiMap::new();
-    equivalences.insert("A", "X");
-    equivalences.insert("B", "Y");
-    equivalences.insert("C", "Z");
-
-    // rules hash designs who wins who
-    // Win = 6 | Draw = 3 | Loss = 0
-    // Scissors = 3 | Paper = 2 | Rock = 1
-    let mut rules = HashMap::new();
-    rules.insert("X", 1);
-    rules.insert("Y", 2);
-    rules.insert("Z", 3);
-
-    // map of who left side wins
-    let mut comparisson_rules = HashMap::new();
-    comparisson_rules.insert("A", "C");
-    comparisson_rules.insert("B", "A");
-    comparisson_rules.insert("C", "B");
-
-    let mut counter = 0;
-
-    for choice in parsed {
-        let opp = choice[0].as_str();
-        let our = equivalences.get_by_right(choice[1].as_str()).unwrap();
-
-        // inserting value of match
-        if &opp == our {
-            counter += 3;
-        } else if comparisson_rules.get(our).unwrap() == &opp {
-            counter += 6;
+impl Outcome {
+    fn inherent_points(self) -> usize {
+        match self {
+            Outcome::Win => 6,
+            Outcome::Draw => 3,
+            Outcome::Loss => 0,
         }
-
-        // inserting value of choice
-        counter += rules.get(choice[1].as_str()).unwrap();
     }
-    println!("{}", counter);
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Move {
+    
+    // returns move value
+    fn inherent_points(self) -> usize {
+        match self {
+            Move::Rock => 1,
+            Move::Paper => 2,
+            Move::Scissors => 3,
+        }
+    }
+    
+    // returns result of matchs given 2 moves
+    // fn outcome(self, theirs: Move) -> Outcome {
+    //     match(self, theirs) {
+    //         (Move::Rock, Move::Paper) => Outcome::Loss,
+    //         (Move::Rock, Move::Rock) => Outcome::Draw,
+    //         (Move::Rock, Move::Scissors) => Outcome::Win,
+    //         (Move::Paper, Move::Scissors) => Outcome::Loss,
+    //         (Move::Paper, Move::Paper) => Outcome::Draw,
+    //         (Move::Paper, Move::Rock) => Outcome::Win,
+    //         (Move::Scissors, Move::Rock) => Outcome::Loss,
+    //         (Move::Scissors, Move::Scissors) => Outcome::Draw,
+    //         (Move::Scissors, Move::Paper) => Outcome::Win,
+    //     }
+    //     
+    // }
+
+    // return what move defeats the other
+    // Self::Rock.beats(Self::Paper) -> returns false beacuse there's no match for this case
+    fn beats(self, other: Move) -> bool {
+        matches!((self, other), (Self::Rock, Self::Scissors) | (Self::Paper, Self::Rock) |  (Self::Scissors, Self::Paper))
+    }
+
+    // return outcome given the comparisson of entries based on beats return
+    fn outcome(self, theirs: Move) -> Outcome {
+        if self.beats(theirs) {
+            Outcome::Win
+        } else if theirs.beats(self) {
+            Outcome::Loss
+        } else {
+            Outcome::Draw
+        }
+    }
+}
+
+// implementing TryFrom for Move, so we can parse from the chars in input file and make them chars
+// into ours types
+impl TryFrom<char> for Move {
+    type Error = color_eyre::Report;
+
+    fn try_from(value: char) -> Result<Self, Self::Error> {
+        match value {
+            'A' | 'X' => Ok(Move::Rock),
+            'B' | 'Y' => Ok(Move::Paper),
+            'C' | 'Z' => Ok(Move::Scissors),
+            _ => Err(color_eyre::eyre::eyre!("not a valid move: {value:?}")),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Round {
+    theirs: Move,
+    ours: Move,
+}
+
+impl Round {
+    // returns outcome based on ours and theirs move
+    fn outcome(self) -> Outcome {
+        self.ours.outcome(self.theirs)
+    }
+
+    fn our_score(self) -> usize {
+        self.ours.inherent_points() + self.outcome().inherent_points()
+    }
+}
+
+// implementing FromStr to our Round, so we can parse a round given an &str (a line):
+impl FromStr for Round {
+    type Err = color_eyre::Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // getting chars from the string(line) input
+        let mut chars = s.chars();
+
+        // let () = () else {}
+        let (Some(theirs), Some(' '), Some(ours), None) = (chars.next(), chars.next(), chars.next(), chars.next()) else {
+            return Err(color_eyre::eyre::eyre!("expected <theirs>SP<ours>EOF, got {s:?}"));
+        };
+
+        // here rust knows that theirs should be a Move, because the struct indicates it, so we need
+        // to turn theirs(char) into our Move, to do so we need to call the TryFrom implementation
+        // that we created before, returning this way our Moves for each player
+        Ok (Self {
+            theirs: theirs.try_into()?,
+            ours: ours.try_into()?,
+        })
+    }
+
+}
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+
+    for round in include_str!("../input.txt").lines().map(|line| line.parse::<Round>())
+    {
+        let round: Round = round?;
+        println!("{round:?}: outcome: {outcome:?}, our score = {our_score:?}", outcome = round.outcome(), our_score = round.our_score());
+    }
+    
+    Ok(())
 }
